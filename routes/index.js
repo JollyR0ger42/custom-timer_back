ec403c81c3e3f26b2a2599cf5804ddaad9f0440c
JollyR0ger <kav993@gmail.com>
2022-04-13 13:02:09 +0300

    add todo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const timer = require('./timer.routes.js')
 const auth = require('./auth.routes.js')
 
+// [KAV]TODO: http codes, and other GOST stuff
 module.exports = (app) => {
   timer(app)
   auth(app)
