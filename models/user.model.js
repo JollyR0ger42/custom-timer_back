93f48c48908e6d8093f87c9e7a141d9c565c290e
JollyR0ger <kav993@gmail.com>
2022-04-14 14:50:46 +0300

    minor fix

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,10 @@
 module.exports = (Sequelize, sequelize) => {
   class User extends Sequelize.Model {}
   User.init({
-    name: {type: Sequelize.STRING},
+    name: {
+      type: Sequelize.STRING,
+      primaryKey: true
+    },
     password: {type: Sequelize.STRING}
   }, {
     sequelize,
