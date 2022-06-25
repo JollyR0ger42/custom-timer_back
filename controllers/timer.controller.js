be310b6d04fd35941780ac1a6b56a10569a30fcc
JollyR0ger <kav993@gmail.com>
2022-06-25 14:08:00 +0300

    minor rework for response

--- controllers/timer.controller.js
+++ controllers/timer.controller.js
@@ -23,15 +23,24 @@ const createTimer = async (payload) => {
 }
 
 const updateById = async (id, payload) => {
+  let result
+  console.log(payload)
   const timer = await Timer.findByPk(id)
+  // for start timer
   if (payload?.started && timer.stopped) {
     await timer.update({started: payload.started, stopped: null})
+  // for stop timer
   } else if (payload?.stopped && !timer.stopped) {
     const timePassed = new Date(payload.stopped) - new Date(timer.started)
     const timeLeft = timer.timeLeft - timePassed
     await timer.update({stopped: payload.stopped, timeLeft})
+  // for upd fields
+  } else {
+    await timer.update(payload)
   }
-  return timer
+  try {result = await Timer.findAll({order: ['name']})}
+  catch (error) {console.error(error)}
+  return result
 }
 
 const deleteById = async (id) => {
